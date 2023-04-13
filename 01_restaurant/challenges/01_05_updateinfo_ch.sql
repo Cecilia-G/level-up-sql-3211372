6c733cece2efd43ff7b4afdacbdfeb8922c74075
Cecilia <96432472+Cecilia-G@users.noreply.github.com>
2023-04-13 18:00:39 +0000

    CHALLENGES 5,6

--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,9 @@
 
 -- Taylor Jenkins, of 27170 6th Ave., Washington, DC,
 -- has moved to 74 Pine St., New York, NY.
+
+SELECT * FROM CUSTOMERS WHERE FIRSTNAME='Taylor';
+
+UPDATE CUSTOMERS 
+SET ADDRESS='74 Pine St.', CITY='New York', STATE='NY'
+WHERE CUSTOMERID=26; 
--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,8 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT * FROM CUSTOMERS WHERE FIRSTNAME='Norby';
+SELECT * FROM RESERVATIONS WHERE CUSTOMERID=64 AND DATE>'2022-07-24';
+
+DELETE FROM RESERVATIONS WHERE ReservationID=2000;
